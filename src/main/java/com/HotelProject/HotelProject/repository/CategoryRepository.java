95eba0d22a2d0f4d6f28cf4d01b019aca8172790
Bourhazi Ahmed <83796717+Bourhazi@users.noreply.github.com>
2024-01-07 20:14:56 +0100

    Delete src directory

--- src/main/java/com/HotelProject/HotelProject/repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.HotelProject.HotelProject.repository;
-
-import com.HotelProject.HotelProject.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category,Long> {
-
-}
